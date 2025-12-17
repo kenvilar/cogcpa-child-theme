b5f8c7f235670dd21c9501f1edecec91fc0a0170
Ken Vilar <kenvilar@gmail.com>
2025-12-17 17:17:14 +0800

    Load shortcodes only if the file exists

--- functions.php
+++ functions.php
@@ -1,4 +1,8 @@
 <?php
 
-// More custom shortcodes
-require_once get_template_directory() . '/shortcodes/index.php';
+$theme_dir = get_stylesheet_directory();
+
+$main_shortcodes = $theme_dir . '/shortcodes/index.php';
+if ( file_exists( $main_shortcodes ) ) {
+	require_once $main_shortcodes;
+}
