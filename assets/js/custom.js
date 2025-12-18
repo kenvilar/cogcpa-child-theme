d66d2f5d022951746f0e5afe29b4103258670140
Ken Vilar <kenvilar@gmail.com>
2025-12-18 23:12:08 +0800

    Add hover animation for `.arrow-sweep-btn-hover` buttons

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -24,7 +24,30 @@ function buttonRingSweepAnimation() {
 				}, DURATION_MS);
 			});
 		});
+		document.querySelectorAll(".arrow-sweep-btn-hover").forEach((btn) => {
+			const sweeps = btn.querySelectorAll(".ring-sweep");
+			const btn_ring = btn.querySelector(".ring");
+			const DURATION_MS = 900; // keep in sync with CSS drawArc (.65s)
 
+			btn.closest('.group').addEventListener("mouseenter", () => {
+				if (btn.classList.contains("is-animating")) return;
+
+				btn.classList.add("is-animating");
+				// btn_ring.style.opacity = "1";
+
+				// restart sweep animations cleanly
+				sweeps.forEach((p) => {
+					p.style.animation = "none";
+					p.getBoundingClientRect(); // reflow
+					p.style.animation = "";
+				});
+
+				setTimeout(() => {
+					btn.classList.remove("is-animating");
+					// btn_ring.style.opacity = "0";
+				}, DURATION_MS);
+			});
+		});
 	});
 }
 
