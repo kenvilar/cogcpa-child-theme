992dd3433aea52287d13ac75b29a3a5c6aefd90e
Ken Vilar <kenvilar@gmail.com>
2025-12-18 23:26:25 +0800

    Change button event listener from click to mouseenter and wrap accordion initialization in DOMContentLoaded event listener

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,7 +5,7 @@ function buttonRingSweepAnimation() {
 			const btn_ring = btn.querySelector(".ring");
 			const DURATION_MS = 900; // keep in sync with CSS drawArc (.65s)
 
-			btn.addEventListener("click", () => {
+			btn.addEventListener("mouseenter", () => {
 				if (btn.classList.contains("is-animating")) return;
 
 				btn.classList.add("is-animating");
@@ -52,28 +52,30 @@ function buttonRingSweepAnimation() {
 }
 
 function myAccordion() {
-	const accordionItemHeaders = document.querySelectorAll(
-		".my-accordion-item-header"
-	);
+	document.addEventListener("DOMContentLoaded", (event) => {
+		const accordionItemHeaders = document.querySelectorAll(
+			".my-accordion-item-header"
+		);
 
-	accordionItemHeaders.forEach((accordionItemHeader) => {
-		accordionItemHeader.addEventListener("click", (event) => {
-			// Uncomment in case you only want to allow for the display of only one collapsed item at a time!
-			const currentlyActiveAccordionItemHeader = document.querySelector(".my-accordion-item-header.active");
-			if (currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader !== accordionItemHeader) {
-				currentlyActiveAccordionItemHeader.classList.toggle("active");
-				currentlyActiveAccordionItemHeader.closest(".my-accordion-item").classList.toggle("active");
-				currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
-			}
+		accordionItemHeaders.forEach((accordionItemHeader) => {
+			accordionItemHeader.addEventListener("click", (event) => {
+				// Uncomment in case you only want to allow for the display of only one collapsed item at a time!
+				const currentlyActiveAccordionItemHeader = document.querySelector(".my-accordion-item-header.active");
+				if (currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader !== accordionItemHeader) {
+					currentlyActiveAccordionItemHeader.classList.toggle("active");
+					currentlyActiveAccordionItemHeader.closest(".my-accordion-item").classList.toggle("active");
+					currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
+				}
 
-			accordionItemHeader.closest(".my-accordion-item").classList.toggle("active");
-			accordionItemHeader.classList.toggle("active");
-			const accordionItemBody = accordionItemHeader.nextElementSibling;
-			if (accordionItemHeader.closest(".my-accordion-item").classList.contains("active")) {
-				accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
-			} else {
-				accordionItemBody.style.maxHeight = 0;
-			}
+				accordionItemHeader.closest(".my-accordion-item").classList.toggle("active");
+				accordionItemHeader.classList.toggle("active");
+				const accordionItemBody = accordionItemHeader.nextElementSibling;
+				if (accordionItemHeader.closest(".my-accordion-item").classList.contains("active")) {
+					accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+				} else {
+					accordionItemBody.style.maxHeight = 0;
+				}
+			});
 		});
 	});
 }
