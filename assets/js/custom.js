315ca4687df2bd7a7e233f05ea5158c75831eb70
Ken Vilar <kenvilar@gmail.com>
2025-12-18 18:58:00 +0800

    Add custom accordion styles and enhanced functionality

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -36,14 +36,15 @@ function myAccordion() {
 	accordionItemHeaders.forEach((accordionItemHeader) => {
 		accordionItemHeader.addEventListener("click", (event) => {
 			// Uncomment in case you only want to allow for the display of only one collapsed item at a time!
-
-			// const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active");
-			// if(currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader!==accordionItemHeader) {
-			//   currentlyActiveAccordionItemHeader.classList.toggle("active");
-			//   currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
-			// }
+			const currentlyActiveAccordionItemHeader = document.querySelector(".my-accordion-item-header.active");
+			if (currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader !== accordionItemHeader) {
+				currentlyActiveAccordionItemHeader.classList.toggle("active");
+				currentlyActiveAccordionItemHeader.closest(".my-accordion-item").classList.toggle("active");
+				currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
+			}
 
 			accordionItemHeader.closest(".my-accordion-item").classList.toggle("active");
+			accordionItemHeader.classList.toggle("active");
 			const accordionItemBody = accordionItemHeader.nextElementSibling;
 			if (accordionItemHeader.closest(".my-accordion-item").classList.contains("active")) {
 				accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
