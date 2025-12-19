1eb6daca94f9279960c7fb8940029a970655e4f4
Ken Vilar <kenvilar@gmail.com>
2025-12-19 16:03:09 +0800

    Refactor `testimonials-slider` shortcode: update image handling logic and clean up layout styling

--- shortcodes/testimonials-slider.php
+++ shortcodes/testimonials-slider.php
@@ -58,10 +58,10 @@ if ( ! function_exists( 'testimonials_slider_shortcode' ) ) {
 							width: auto;
 							fill: none;
 						}
-            .testimonials-slider {
-              display: flex;
-              flex-wrap: nowrap;
-            }
+						.testimonials-slider {
+							display: flex;
+							flex-wrap: nowrap;
+						}
           </style>
           <div id="testimonials-slider" class="splide" role="group" aria-label="Testimonials Slider">
             <div class="splide__track">
@@ -114,14 +114,6 @@ if ( ! function_exists( 'testimonials_slider_shortcode' ) ) {
 				  $testimonials_query->the_post();
 				  ?>
                 <div class="testimonial-item">
-					<?php
-					if ( has_post_thumbnail() ) : ?>
-                      <div class="testimonial-image">
-						  <?php
-						  the_post_thumbnail( 'full' ); ?>
-                      </div>
-					<?php
-					endif; ?>
                   <svg width="19" height="17" viewBox="0 0 19 17" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path
                       d="M3.36362 17C2.3953 17 1.57988 16.568 0.91735 15.704C0.305783 14.7892 0 13.5187 0 11.8924C0 9.1988 0.560603 6.8864 1.68181 4.95516C2.85398 2.97309 4.45934 1.32137 6.49789 0L7.87392 1.75336C6.19211 3.12556 4.91801 4.52317 4.05163 5.94619C3.18524 7.31838 2.75205 9.07175 2.75205 11.2063C2.85398 11.1555 3.05783 11.13 3.36362 11.13C4.12807 11.13 4.81609 11.3587 5.42765 11.8161C6.09018 12.2735 6.42145 12.9851 6.42145 13.9507C6.42145 14.9163 6.11567 15.6786 5.5041 16.2377C4.9435 16.7459 4.23 17 3.36362 17ZM14.1425 17C13.1742 17 12.3587 16.568 11.6962 15.704C11.0846 14.7892 10.7789 13.5187 10.7789 11.8924C10.7789 9.1988 11.3395 6.8864 12.4607 4.95516C13.6328 2.97309 15.2382 1.32137 17.2768 0L18.6528 1.75336C16.971 3.12556 15.6969 4.52317 14.8305 5.94619C13.9641 7.31838 13.5309 9.07175 13.5309 11.2063C13.6328 11.1555 13.8367 11.13 14.1425 11.13C14.9069 11.13 15.5949 11.3587 16.2065 11.8161C16.869 12.2735 17.2003 12.9851 17.2003 13.9507C17.2003 14.9163 16.8945 15.6786 16.283 16.2377C15.7224 16.7459 15.0089 17 14.1425 17Z"
@@ -142,8 +134,18 @@ if ( ! function_exists( 'testimonials_slider_shortcode' ) ) {
                     </h3>
                     <div class="w-[1px] h-[25px] bg-darkblue"></div>
                     <div class="max-w-[107px] w-full">
-                      <img src="/wp-content/uploads/highground-trading-group.avif" class="w-full h-full object-cover"
-                           alt="logo">
+						<?php
+						if ( has_post_thumbnail() ) : ?>
+                          <img src="<?php
+						  the_post_thumbnail_url( 'full' ); ?>" class="w-full h-full object-cover"
+                               alt="logo">
+						<?php
+						else: ?>
+                          <img src="/wp-content/uploads/highground-trading-group-logo.avif"
+                               class="w-full h-full object-cover"
+                               alt="logo">
+						<?php
+						endif; ?>
                     </div>
                   </div>
                 </div>
